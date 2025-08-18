a1c86df9d791cd822fa068556d8c2cd67aa8c817
AhilyaKokare <ahilyakokare13@gmail.com>
2025-08-18 19:21:40 +0530

    Implemented core functionality and UI

--- frontend/Visitor_Pass_Management_System/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://192.168.2.15:8080',
-  // notificationApiBaseUrl: 'http://192.168.2.15:8081',
-};
--- frontend/Visitor_Pass_Management_System/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://192.168.2.15:8080', // Core visitor-pass-service
-  // notificationApiBaseUrl: 'http://192.168.2.15:8081', // Notification-service (optional direct calls)
-};
